d7eac59d3919e816a7e17413cce3e70eecc65f70
ghostin <9401642665tk@gmail.com>
2024-01-02 22:36:33 +0530

    File Upld
    Initial File Set up
    (1st File Upload)

---
+++ dd.php
@@ -0,0 +1,25 @@
+<form action="#" method="post" class="form-inline">
+<input type="search" placeholder="Enter Your Domain Name Here" type="text" class="form-control" name="domain_name">
+<select name="suffix" id="domainExtension">
+<option value=".com">.com</option>
+<option value=".net">.net</option>
+<option value=".org">.org</option>
+<option value=".biz">.biz</option>
+<option value=".info">.info</option>
+<option value=".in">.in</option>
+<option value=".us">.us</option>
+<option value=".uk">.uk</option>
+<option value=".au">.au</option>
+<option value=".co">.co</option>
+<option value=".gov">.gov</option>
+<option value=".tech">.tech</option>
+<option value=".online">.online</option>
+<option value=".pro">.pro</option>
+<option value=".io">.io</option>
+<option value=".tv">.tv</option>
+<option value=".live">.live</option>
+<option value=".app">.app</option>
+</select>
+
+<input type="submit" class="btn btn-primary" name="check" value="Check">
+</form>
---
+++ index.php
@@ -0,0 +1,627 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="description" content>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<title>PLAK - Web Hosting and Designing</title>
+
+<link rel="icon" href="images/4ubqvDaP2y02.png">
+
+<link rel="stylesheet" href="css/3VzEWHIWh2ch.css">
+<link rel="stylesheet" href="./font-awesome-4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+
+<div id="preloader">
+<div class="loader"></div>
+</div>
+
+
+<header class="header-area">
+
+<div class="top-header-area">
+<div class="container">
+<div class="row">
+<div class="col-6">
+<div class="top-header-content">
+<a href="#"><i class="fa fa-phone" aria-hidden="true"></i> <span>Call Us: +91 60031 62579</span></a>
+<a href="#"><i class="fa fa-envelope" aria-hidden="true"></i> <span>Email: mail@plak.in</span></a>
+</div>
+</div>
+<div class="col-6">
+<div class="top-header-content">
+
+<a href="./ca.html"><i class="fa fa-lock" aria-hidden="true"></i> <span>Login / Register</span></a>
+<a href="./ca.html"><i class="fa fa-tasks" aria-hidden="true"></i><span>Client Area</span></a>
+<div class="dropdown">
+<a class="btn pr-0 dropdown-toggle" href="#" role="button" id="langdropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="images/C6bVNj3gUtBD.png" alt> English</a>
+<div class="dropdown-menu dropdown-menu-right" aria-labelledby="langdropdown">
+<a class="dropdown-item" href="../as">- Assamese</a>
+<a class="dropdown-item" href="../hi">- Hindi</a>
+<a class="dropdown-item" href="../bl">- Bangla</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="main-header-area">
+<div class="classy-nav-container breakpoint-off">
+<div class="container">
+
+<nav class="classy-navbar justify-content-between" id="hamiNav">
+
+<a class="nav-brand" href="index.html"><img src="images/gkh7iNlLbvpQ.png" alt></a>
+
+<div class="classy-navbar-toggler">
+<span class="navbarToggler"><span></span><span></span><span></span></span>
+</div>
+
+<div class="classy-menu">
+
+<div class="classycloseIcon">
+<div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+</div>
+
+<div class="classynav">
+<ul id="nav">
+<li class="active"><a href="./index.html">Home</a></li>
+<li><a href="./Web-Designing.html">Web-Designing</a></li>
+
+<li><a href="./hosting.html">Hosting</a></li>
+<li><a href="#">Other Services</a>
+  <ul class="dropdown">
+  <li><a href="./domin.html">- Domin</a></li>
+  
+  <li><a href="./coming-soon.html">- App Developement</a></li>
+  <li><a href="./coming-soon.html">- Server Mangement</a></li>
+  
+  <li><a href="#">- VPS</a>
+  <ul class="dropdown">
+  <li><a href="./vps.html?unmanaged">- Unmanaged VPS</a></li>
+  <li><a href="./vps.html?managed">- Managed VPS</a></li>
+  </ul>
+  </li>
+  <li><a href="#">- Server</a>
+    <ul class="dropdown">
+    <li><a href="./server.html?unmanaged">- Unmanaged Barebome Server</a></li>
+    <li><a href="./server.html?managed">- Managed Server</a></li>
+    </ul>
+    </li>
+  </ul>
+  </li>
+<li><a href="./about.html">About</a></li>
+<li><a href="./contact/">Contact</a></li>
+</ul>
+
+<div class="live-chat-btn ml-5 mt-4 mt-lg-0 ml-md-4">
+<a href="./chat.html" class="btn hami-btn live--chat--btn"><i class="fa fa-comments" aria-hidden="true"></i> Live Chat</a>
+</div>
+</div>
+
+</div>
+</nav>
+</div>
+</div>
+</div>
+</header>
+
+
+<section class="welcome-area">
+
+<div class="welcome-pattern">
+<img src="images/NG7KrRlvC2fl.png" alt>
+</div>
+
+<div class="welcome-slides owl-carousel">
+
+<div class="single-welcome-slide">
+
+<div class="welcome-content h-100">
+<div class="container h-100">
+<div class="row h-100 align-items-center">
+
+<div class="col-12 col-md-9 col-lg-7">
+<div class="welcome-text mb-50">
+<h2 data-animation="fadeInLeftBig" data-delay="200ms" data-duration="1s">The Best <br> Web Design</h2>
+<h3 data-animation="fadeInUpBig" data-delay="400ms" data-duration="1s">Starting at <span>₹6999</span> ₹899*</h3>
+<p data-animation="fadeInLeftBig" data-delay="600ms" data-duration="1s">Everything you will EVER need for your online Buisness!</p>
+<a href="./webd.html" class="btn hami-btn btn-2" data-animation="fadeInLeftBig" data-delay="800ms" data-duration="1s">Get Start Now!</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="welcome-thumbnail">
+<img src="images/SaojuafCMg2t.png" alt>
+</div>
+</div>
+
+<div class="single-welcome-slide">
+
+<div class="welcome-content h-100">
+ <div class="container h-100">
+<div class="row h-100 align-items-center">
+
+<div class="col-12 col-md-9 col-lg-7">
+<div class="welcome-text mb-50">
+<h2 data-animation="fadeInUpBig" data-delay="200ms" data-duration="1s">The Best <br> Web Hosting</h2>
+<h3 data-animation="fadeInUpBig" data-delay="400ms" data-duration="1s">Starting at <span>₹899</span> ₹83/month*</h3>
+<p data-animation="fadeInUpBig" data-delay="600ms" data-duration="1s">Everything you will EVER need to Host and Manage your Website!</p>
+<a href="./hosting.html" class="btn hami-btn btn-2" data-animation="fadeInUpBig" data-delay="800ms" data-duration="1s">Get Start Now!</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="welcome-thumbnail">
+<img src="images/VkqLUKRZN9sD.png" alt>
+</div>
+</div>
+</div>
+
+<div class="clouds">
+<img src="images/n9IWTTadgfVH.png" alt class="cloud-1">
+<img src="images/w6IpnAKci0pN.png" alt class="cloud-2">
+<img src="images/hmV2JgLx58yJ.png" alt class="cloud-3">
+<img src="images/QVKn52I7j2NZ.png" alt class="cloud-4">
+<img src="images/zsxyEjNVFgtD.png" alt class="cloud-5">
+</div>
+</section>
+
+
+
+<section class="find-domain-area section-padding-100-0">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-12 col-md-4">
+<div class="domain-text mb-100">
+<h2>Find Your Perfect Domain Name</h2>
+<h6>Only ₹150 for the first year*</h6>
+</div>
+</div>
+<div class="col-12 col-md-8">
+<div class="domain-search-form mb-100">
+
+
+<form action="#" method="post" class="form-inline">
+<input type="search" placeholder="Enter Your Domain Name Here" type="text" class="form-control" name="domain_name">
+<select name="suffix" id="domainExtension">
+<option value=".com">.com</option>
+<option value=".net">.net</option>
+<option value=".org">.org</option>
+<option value=".biz">.biz</option>
+<option value=".info">.info</option>
+<option value=".in">.in</option>
+<option value=".us">.us</option>
+<option value=".uk">.uk</option>
+<option value=".au">.au</option>
+<option value=".co">.co</option>
+<option value=".gov">.gov</option>
+<option value=".tech">.tech</option>
+<option value=".online">.online</option>
+<option value=".pro">.pro</option>
+<option value=".io">.io</option>
+<option value=".tv">.tv</option>
+<option value=".live">.live</option>
+<option value=".app">.app</option>
+</select>
+<button type="submit" name="check" value="Check">Search Domain</button>
+</form>
+<?php
+ 
+ if(isset($_POST['check'])) {
+  
+  if (!empty($_POST['domain_name'])){
+  $name_domain = trim($_POST['domain_name']).$_POST['suffix'];
+ 
+    $godaddycheck = 'https://in.godaddy.com/domains/searchresults.aspx?checkAvail=1&tmskey=&domainToCheck='.$name_domain.'';
+    $namecomcheck = 'https://www.name.com/domain/search/'.$name_domain.'';
+    $registercomcheck = 'http://www.register.com/domain/search/wizard.rcmx?searchDomainName='.$name_domain.'&searchPath=Default&searchTlds=';
+  if ( gethostbyname($name_domain) != $name_domain ) {
+  echo "<H3 style='color:red;'>Domain $name_domain has taken.</H3>";
+  
+  }else{
+  echo "<H3 style='color:green;' >Domain $name_domain is available.</H3>
+  <a href='./dd.php#$name_domain' class='btn hami-btn'>Buy it Now!</a>
+";
+  }
+  }
+  else {
+  echo "<H3 style='color:red;'>Error: Enter Domain Name.</H3>";
+  }
+ }
+ ?>
+
+<div class="domain-price-help mt-50 d-flex align-items-center justify-content-between">
+<p>.IN ₹500</p>
+<p>.CO.IN ₹400</p>
+<p>.ONLINE ₹200</p>
+<p>.COM ₹975</p>
+<p>.NET ₹945</p>
+<p>.ORG ₹750</p>
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</section>
+
+
+<section class="hami-features-area bg-gray section-padding-100">
+<div class="container">
+<div class="row">
+
+<div class="col-12">
+ <div class="section-heading text-center">
+<h2>Overall Features</h2>
+<p>Our revolutionary Cloud solution is powerful, simple, and surprisingly affordable.</p>
+</div>
+</div>
+</div>
+<div class="row">
+
+<div class="col-12 col-sm-6 col-lg-4">
+<div class="single-feature-area d-flex mb-50">
+<div class="feature-icon">
+<i class="icon_cloud-upload_alt"></i>
+</div>
+<div class="feature-text">
+<h5>Auto Updates</h5>
+<p>Don't be distracted by regular updates. We will do that for you.*</p>
+</div>
+</div>
+</div>
+
+<div class="col-12 col-sm-6 col-lg-4">
+<div class="single-feature-area d-flex mb-50">
+<div class="feature-icon">
+<i class="icon_adjust-vert"></i>
+</div>
+<div class="feature-text">
+<h5>Optimized Software</h5>
+<p>Our Server and network are Optimized by specialist's for eficient fast workflow.</p>
+</div>
+</div>
+</div>
+
+<div class="col-12 col-sm-6 col-lg-4">
+<div class="single-feature-area d-flex mb-50">
+<div class="feature-icon">
+<i class="icon_archive_alt"></i>
+</div>
+<div class="feature-text">
+<h5>Daily Backups</h5>
+<p>Be worryfree as we Keep Daily Backups for you.*</p>
+</div>
+</div>
+</div>
+
+<div class="col-12 col-sm-6 col-lg-4">
+<div class="single-feature-area d-flex mb-50">
+<div class="feature-icon">
+<i class="icon_globe-2"></i>
+</div>
+<div class="feature-text">
+<h5>Wide Networking</h5>
+<p>Witg help of Cloudflare Tunneling and DNS get the worlds latest cutting edge networking technology for your website.</p>
+</div>
+</div>
+</div>
+
+<div class="col-12 col-sm-6 col-lg-4">
+<div class="single-feature-area d-flex mb-50">
+<div class="feature-icon">
+<i class="icon_shield"></i>
+</div>
+<div class="feature-text">
+<h5>Protected</h5>
+<p>You website's are protected from malware and spam by top of line protections.</p>
+</div>
+</div>
+</div>
+
+<div class="col-12 col-sm-6 col-lg-4">
+<div class="single-feature-area d-flex mb-50">
+<div class="feature-icon">
+<i class="icon_headphones"></i>
+</div>
+<div class="feature-text">
+<h5>Free Support</h5>
+<p>Get 24*7 Suuport for FREE for any help or querries.*</p>
+</div>
+</div>
+</div>
+</div>
+</div> 
+
+<div class="feature-pattern">
+<img src="images/NG7KrRlvC2fl.png" alt>
+</div>
+</section>
+
+
+<section class="hami-price-plan-area mt-50">
+<div class="container">
+<div class="row">
+
+<div class="col-12">
+<div class="section-heading text-center">
+<h2>Choose Your Web Hosting Plan</h2>
+<p>You want custom hosting plan. No hidden charge.</p>
+</div>
+</div>
+</div>
+<div class="row justify-content-center">
+
+<div class="col-12 col-md-6 col-lg-4">
+<div class="single-price-plan mb-100">
+
+<div class="price-plan-title">
+<h4>Standard Hosting</h4>
+<p>On sale - Save 50%</p>
+</div>
+
+<div class="price-plan-value">
+<h2><span>₹</span>83</h2>
+<p>/per month</p>
+</div>
+
+<a href="https://client.plak.in/order/main/index/Hosting" class="btn hami-btn w-100 mb-30">Get Started</a>
+
+<div class="price-plan-desc">
+<p><i class="icon_check"></i> 1 Number of Website</p>
+<p><i class="icon_check"></i> 5 Subdomin</p>
+<p><i class="icon_check"></i> 5 Email Accounts</p>
+<p><i class="icon_check"></i> 5 Database</p>
+<p><i class="icon_check"></i> Direct Admin CP</p>
+<p><i class="icon_check"></i> 100 GB Bandwidth</p>
+<p><i class="icon_check"></i> 2X Processing Power &amp; Memory</p>
+</div>
+
+
+</div>
+</div>
+
+<div class="col-12 col-md-6 col-lg-4">
+<div class="single-price-plan active mb-100">
+
+<div class="popular-tag">
+<i class="icon_star"></i> Best Plan <i class="icon_star"></i>
+</div>
+
+<div class="price-plan-title">
+<h4>Advanced Hosting</h4>
+<p>On sale - Save 70%</p>
+</div>
+
+<div class="price-plan-value">
+<h2><span>₹</span>119</h2>
+<p>/per month</p>
+</div>
+
+<a href="https://client.plak.in/order/main/index/Hosting" class="btn hami-btn w-100 mb-30">Get Started</a>
+
+<div class="price-plan-desc">
+<p><i class="icon_check"></i> 5 Number of Website</p>
+<p><i class="icon_check"></i> 10 Subdomin</p>
+<p><i class="icon_check"></i> 10 Email Accounts</p>
+<p><i class="icon_check"></i> 10 Database</p>
+<p><i class="icon_check"></i> Direct Admin CP</p>
+<p><i class="icon_check"></i> 400 GB Bandwidth</p>
+<p><i class="icon_check"></i> 5X Processing Power &amp; Memory</p>
+</div>
+
+</div>
+</div>
+
+<div class="col-12 col-md-6 col-lg-4">
+<div class="single-price-plan mb-100">
+ 
+<div class="price-plan-title">
+<h4>Unlimited Hosting</h4>
+<p>On sale - Save 50%</p>
+</div>
+
+<div class="price-plan-value">
+<h2><span>₹</span>250</h2>
+<p>/per month</p>
+</div>
+
+<a href="https://client.plak.in/order/main/index/Hosting" class="btn hami-btn w-100 mb-30">Get Started</a>
+
+<div class="price-plan-desc">
+<p><i class="icon_check"></i> Unlimited Number of Website</p>
+<p><i class="icon_check"></i> Unlimited Subdomin</p>
+<p><i class="icon_check"></i> Unlimited Email Accounts</p>
+<p><i class="icon_check"></i> Unlimited Database</p>
+<p><i class="icon_check"></i> Direct Admin CP</p>
+<p><i class="icon_check"></i> Unlimited Bandwidth</p>
+<p><i class="icon_check"></i> 10X Processing Power &amp; Memory</p>
+</div>
+
+
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<section class="hami-call-to-action bg-gray section-padding-100-0">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-12 col-md-6">
+<div class="cta-thumbnail mb-100">
+<img src="images/VkqLUKRZN9sD.png" alt>
+</div>
+</div>
+<div class="col-12 col-md-6">
+<div class="cta--content mb-100"><span>Up to 70% Discount with FREE Domain Name Registration Included!</span>
+<h2>Delux ONLINE KIT</h2>
+
+<div class="cta-desc mb-50">
+<h6><i class="icon_check"></i> Domain</h6>
+<h6><i class="icon_check"></i> Hosting</h6>
+<h6><i class="icon_check"></i> Web-Design</h6>
+<h6><i class="icon_check"></i> FREE SEO</h6>
+<h6><i class="icon_check"></i> FREE 24*7 Support</h6>
+</div>
+
+<a href="./sppack.html" class="btn hami-btn">Get Start Now!</a>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<section class="hami-support-area bg-gray">
+<div class="container">
+<div class="row">
+<div class="col-12">
+<div class="support-text">
+<h2>Need help? Call our support team: +91 6003162579</h2>
+</div>
+</div>
+</div>
+</div>
+
+<div class="support-pattern" style="background-image: url(images/RU01fvb3uIEq.png);"></div>
+</section>
+
+
+<section class="hami-cta-area">
+<div class="container">
+<div class="cta-text">
+<h2>A company of <span>ASSAM</span> INDIA</h2>
+</div>
+</div>
+</section>
+
+
+<footer class="footer-area section-padding-80-0">
+
+<div class="main-footer-area">
+<div class="container">
+<div class="row justify-content-between">
+
+<div class="col-6 col-sm-4 col-lg-2">
+<div class="single-footer-widget mb-80">
+
+<h5 class="widget-title">Products</h5>
+
+<ul class="footer-nav">
+<li><a href="./hosting.html">Shared hosting</a></li>
+<li><a href="./hosting.html">WordPress hosting</a></li>
+<li><a href="./vps.html">Vps hosting</a></li>
+<li><a href="./server.html">Dedicated hosting</a></li>
+<li><a href="./coming-soon.html">Reseller hosting</a></li>
+<li><a href="./hosting.html">Hosting features</a></li>
+</ul>
+</div>
+</div>
+
+<div class="col-6 col-sm-4 col-lg-2">
+<div class="single-footer-widget mb-80">
+
+<h5 class="widget-title">Programs</h5>
+
+<ul class="footer-nav">
+<li><a href="https://make.wordpress.org/">WordPress</a></li>
+<li><a href="./about.html">Affiliates</a></li>
+<li><a href="https://wordpress.org/about/">WordPress guide</a></li>
+
+</ul>
+</div>
+</div>
+
+
+
+<div class="col-6 col-sm-4 col-lg-2">
+<div class="single-footer-widget mb-80">
+
+<h5 class="widget-title">Company</h5>
+
+<ul class="footer-nav">
+<li><a href="./about.html">About</a></li>
+<li><a href="./coming-soon.html">Careers</a></li>
+<li><a href="./contact/index.html">Contact</a></li>
+<li><a href="./tm.html">Terms of service</a></li>
+<li><a href="./pp.html">Privacy policy</a></li>
+
+</ul>
+</div>
+</div>
+
+<div class="col-12 col-sm-8 col-lg-4">
+<div class="single-footer-widget mb-80">
+<img src="./images/logos.png" alt="payment accepted">
+<h5 class="widget-title">INDIA</h5>
+<p>Amazing hosting at Affordable Price.</p>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+
+<div class="bottom-footer-area bg-gray">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-12 col-md-6">
+
+<div class="copywrite-text">
+<p>
+Copyright &copy;<script data-cfasync="false" src="js/cY0grcNBbUMx.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | PLAK IN</a>
+
+</p>
+</div>
+</div>
+<div class="col-12 col-md-6">
+<div class="payments-methods d-flex align-items-center">
+<p>We Accept</p>
+<img src="./images/payments.png" alt="payment accepted">
+<p><a href="./payment.html"> And More</a></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</footer>
+
+
+
+<script src="js/H1MvzSq4hFrT.js"></script>
+
+<script src="js/r4d0Piw4DBsF.js"></script>
+
+<script src="js/uL0sF0ZEtn5x.js"></script>
+
+<script src="js/CZvF0ZfKGxhq.js"></script>
+
+<script src="js/WpFxm7xSAWen.js"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v52afc6f149f6479b8c77fa569edb01181681764108816" data-cf-beacon="{"rayId":"7d7d38b22bdab794","version":"2023.4.0","b":1,"token":"cd0b4b3a733644fc843ef0b185f98241","si":100}" crossorigin="anonymous"></script>
+</body>
+</html>
